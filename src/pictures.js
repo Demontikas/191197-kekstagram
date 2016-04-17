bd339432325fa3b6557783da9de61f94caf691a2
Demontikas <marsikevgeni@mail.ru>
2016-04-17 15:59:07 +0300

    module5-task1
    1. Загрузка данных по XMLHttpRequest
    2. Написаны обработчики событий для фильтров

--- src/pictures.js
+++ src/pictures.js
@@ -1,6 +1,8 @@
 'use strict';
 
-document.querySelector('.filters').classList.add('hidden');
+var formFilters = document.querySelector('.filters');
+formFilters.classList.remove('hidden');
+var filters = formFilters.querySelectorAll('input');
 var picturesContainer = document.querySelector('.pictures');
 var templateElement = document.querySelector('#picture-template');
 var elementToClone;
@@ -36,7 +38,92 @@ var getPictureElement = function(data, container) {
   return element;
 };
 
-window.pictures.forEach(function(picture) {
-  getPictureElement(picture, picturesContainer);
+var renderPictures = function(pictures) {
+  picturesContainer.innerHTML = '';
+  pictures.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
+};
+var getPictures = function(callback) {
+
+  var xhr = new XMLHttpRequest();
+  xhr.onerror = function() {
+    picturesContainer.classList.add('pictures-failure');
+  };
+  xhr.timeout = 10000;
+  xhr.ontimeout = function() {
+    picturesContainer.classList.add('pictures-failure');
+  };
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      picturesContainer.classList.remove('pictures-loading');
+      if (xhr.status !== 200) {
+        picturesContainer.classList.add('pictures-failure');
+      }
+    } else {
+      picturesContainer.classList.add('pictures-loading');
+    }
+  };
+  xhr.onload = function(evt) {
+    try {
+      var loadedData = JSON.parse(evt.target.response);
+      callback(loadedData);
+    } catch(e) {
+      picturesContainer.classList.add('pictures-failure');
+    }
+  };
+
+  xhr.open('GET', '//o0.github.io/assets/json/pictures.json');
+  xhr.send();
+};
+var pictures;
+getPictures(function(loadedPictures) {
+  pictures = loadedPictures;
+  setFiltersEnabled(true);
+  setFilterEnabled(DEFAULT_FILTER);
 });
+
+var Filter = {
+  'POPULAR': 'filter-popular',
+  'NEW': 'filter-new',
+  'DISCUSSED': 'filter-discussed'
+};
+var DEFAULT_FILTER = Filter.POPULAR;
+var setFiltersEnabled = function(enabled) {
+  for (var i = 0; i < filters.length; i++) {
+    filters[i].onclick = enabled ? function() {
+      setFilterEnabled(this.id);
+    } : null;
+  }
+};
+var setFilterEnabled = function(filter) {
+  var filteredPictures = getFilteredPictures(pictures, filter);
+  renderPictures(filteredPictures);
+};
+var getFilteredPictures = function(hotels, filter) {
+  var picturesToFilter = pictures.slice(0);
+
+  switch (filter) {
+    case Filter.DISCUSSED:
+      picturesToFilter.sort(function(a, b) {
+        return b.comments - a.comments;
+      });
+      break;
+    case Filter.NEW:
+      picturesToFilter.sort(function(a, b) {
+        var dateOne = new Date(a.date);
+        var dateTwo = new Date(b.date);
+        return dateTwo - dateOne;
+      });
+      break;
+    case Filter.POPULAR:
+      picturesToFilter.sort(function(a, b) {
+        return b.likes - a.likes;
+      });
+      break;
+    default:
+      break;
+  }
+  return picturesToFilter;
+};
 
