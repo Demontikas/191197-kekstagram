2facd1e33224a6c6d8e88e3add5a7905a1090f74
Demontikas <marsikevgeni@mail.ru>
2016-05-05 01:17:53 +0300

    module10-task1
    создан конструктор PhotoInfo, обработчик клика по лайкам.

--- src/gallery.js
+++ src/gallery.js
@@ -37,10 +37,13 @@ Gallery.prototype._onDocumentKeyDown = function(evt) {
 Gallery.prototype._onPhotoClick = function(evt) {
   if (evt.target.classList.contains('gallery-overlay') || evt.target.classList.contains('gallery-overlay-close')) {
     this.forHide();
-  } else {
-    if (this.indexOfArray < this.galleryPictures.length - 1) {
-      this.forShow(++this.indexOfArray);
-    }
+  } else if(evt.target.classList.contains('likes-count')) {
+    var pictureNode = document.querySelector('.pictures');
+    var pictureList = pictureNode.querySelectorAll('.picture');
+    pictureList[this.indexOfArray].querySelector('.picture-likes').click();
+    evt.target.textContent = this.galleryPictures[this.indexOfArray].likes;
+  } else if (this.indexOfArray < this.galleryPictures.length - 1) {
+    this.forShow(++this.indexOfArray);
   }
 };
 Gallery.prototype.forHide = function() {
--- src/photo.js
+++ src/photo.js
@@ -2,17 +2,34 @@
 var imageTo = require('./pictures');
 /**
  * @param {Object} data
+ * @constructor
+ */
+var PhotoInfo = function(data) {
+  this.setLikesCount = function(element) {
+    element.textContent = ++data.likes;
+  };
+};
+/**
+ * @param {Object} data
  * @param {Node} container
  * @constructor
  */
 var Photo = function(data, container) {
   this.data = data;
+  this.PhotoInfo = new PhotoInfo(data);
   this.element = imageTo.getPictureElement(this.data, container);
+  this.pictureLikes = this.element.querySelector('.picture-likes');
   this.container = container;
   this.container.appendChild(this.element);
+  this.pictureLikes.addEventListener('click', this.likesClick.bind(this));
 };
 Photo.prototype.remove = function() {
   this.container.removeChild(this.element);
+  this.pictureLikes.removeEventListener('click', this.likesClick.bind(this));
+};
+Photo.prototype.likesClick = function(evt) {
+  evt.preventDefault();
+  this.PhotoInfo.setLikesCount(this.pictureLikes);
 };
 module.exports = Photo;
 
